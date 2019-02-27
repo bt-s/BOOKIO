a5fac2ac3d5f0868b55c44f4ad9de076360f9d3a
Bas Straathof <bas_straathof@msn.com>
2019-02-27 14:20:33 +0100

    Implement authentication API for the Firebase class

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const devConfig = {
   apiKey: process.env.REACT_APP_DEV_API_KEY,
@@ -14,7 +15,22 @@ const config = process.env.NODE_ENV === 'development' ? devConfig : null;
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.auth = app.auth();
   }
+
+  // *** Auth API ***
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
