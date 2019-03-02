719001ced594eabc0222ca9eb19fdcfd1890df46
Bas Straathof <bas_straathof@msn.com>
2019-03-02 13:19:53 +0100

    Rewrite Form component using React hooks

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,55 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import uuidv1 from 'uuid';
 import {addBook} from '../../redux/actions/index';
+
+const ConnectedForm = props => {
+  const [title, setTitle] = useState('');
+
+  const handleChange = e => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const id = uuidv1();
+    props.addBook({title, id});
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" value={title} onChange={handleChange} />
+      </div>
+      <button type="submit">SAVE</button>
+    </form>
+  );
+};
 
 function mapDispatchToProps(dispatch) {
   return {
     addBook: book => dispatch(addBook(book))
   };
-}
-
-class ConnectedForm extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      title: ''
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({[e.target.id]: e.target.value});
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    const {title} = this.state;
-    const id = uuidv1();
-    this.props.addBook({title, id});
-  }
-
-  render() {
-    const {title} = this.state;
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={this.handleChange}
-          />
-        </div>
-        <button type="submit">SAVE</button>
-      </form>
-    );
-  }
 }
 
 const Form = connect(
