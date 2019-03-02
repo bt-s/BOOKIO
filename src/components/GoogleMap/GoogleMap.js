78a1970dcadf5cae19c6059c0d097847e7c8920b
Bas Straathof <bas_straathof@msn.com>
2019-03-02 13:21:11 +0100

    Turn GoogleMap component into a functional component

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -8,31 +8,29 @@ import Loader from '../Loader/Loader';
 
 const LoadingContainer = props => <Loader />;
 
-export class MapContainer extends React.Component {
-  render() {
-    const style = {
-      width: '300px',
-      height: '300px'
-    };
+export const MapContainer = props => {
+  const style = {
+    width: '300px',
+    height: '300px'
+  };
 
-    const initialCoordinates = {
-      lat: 59.3498,
-      lng: 18.0707
-    };
+  const initialCoordinates = {
+    lat: 59.3498,
+    lng: 18.0707
+  };
 
-    const locationName = 'KTH, Stockholm';
+  const locationName = 'KTH, Stockholm';
 
-    return (
-      <Map
-        google={this.props.google}
-        style={style}
-        initialCenter={initialCoordinates}
-        zoom={12}>
-        <Marker name={locationName} />
-      </Map>
-    );
-  }
-}
+  return (
+    <Map
+      google={props.google}
+      style={style}
+      initialCenter={initialCoordinates}
+      zoom={12}>
+      <Marker name={locationName} />
+    </Map>
+  );
+};
 
 const GoogleMap = GoogleApiWrapper({
   apiKey: googleAPIKey,
