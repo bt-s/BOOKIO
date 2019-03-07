4b5e65d65bcff45769917ea77827d94851b7c280
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-03-07 17:37:10 +0100

    add npm package axios, add several action types for userBook and book state, add userBook and book reducers to reducer index

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,15 @@ import {combineReducers} from 'redux';
 import sessionReducer from './session';
 import userReducer from './user';
 import messageReducer from './message';
+import userBookReducer from './userBook';
+import bookReducer from './book';
 
 const rootReducer = combineReducers({
   sessionState: sessionReducer,
   userState: userReducer,
-  messageState: messageReducer
+  messageState: messageReducer,
+  userBookState: userBookReducer,
+  bookState: bookReducer
 });
 
 export default rootReducer;
