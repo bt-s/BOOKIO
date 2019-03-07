6109bf0467c45aaf2871220a6623aa363d2956f2
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-03-07 15:52:54 +0100

    add redux action file to handle function for getting book title suggestion

---
+++ src/redux/actions/bookTitleSuggestions.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import {
+  BOOK_TITLE_SUGGESTION_ERROR,
+  BOOK_TITLE_SUGGESTION_LOADING,
+  BOOK_TITLE_SUGGESTION_SUCCESS
+} from '../constants/action-types';
+
+const loading = () => {
+  return {
+    type: BOOK_TITLE_SUGGESTION_LOADING
+  };
+};
+
+const success = (payload) => {
+  return {
+    type: BOOK_TITLE_SUGGESTION_SUCCESS,
+    payload
+  };
+};
+
+const error = () => {
+  return {
+    type: BOOK_TITLE_SUGGESTION_ERROR
+  };
+};
+
+export const fetchBookTitleSuggestions = (payload) => {
+  return (dispatch) => {
+    dispatch(loading());
+    axios({
+      method: 'GET',
+      url: `https://www.goodreads.com/book/auto_complete?format=json&q=${payload}`
+    })
+      .then((res) => {
+        console.log(res);
+        dispatch(success(res.data));
+      })
+      .catch((err) => {
+        dispatch(error());
+      })
+  };
+};
