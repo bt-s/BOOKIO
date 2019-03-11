6495d9be727e2022a3ea3d6ac54ea3eaf1950d75
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-03-11 16:44:37 +0100

    add pages folder that will contain page files

---
+++ src/pages/AddNewBook.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AddNewBookForm from '../components/AddNewBookForm/AddNewBookForm';
+
+class AddNewBook extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <AddNewBookForm />
+      </React.Fragment>
+    )
+  };
+};
+
+export default AddNewBook;
