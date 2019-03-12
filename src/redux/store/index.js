392f7f1aeb47204cc167dcdb2392e789a8b500fc
Bas Straathof <bas_straathof@msn.com>
2019-03-12 10:01:04 +0100

    Fix Firebox bug
    The error was: It looks like you are passing several store
    enhancers to createStore()

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,11 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
