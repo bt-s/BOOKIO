2fb9cad76a5f178e823daaf1249bdd8d947ae590
Bas Straathof <bas_straathof@msn.com>
2019-03-19 22:03:47 +0100

    Lint Rating component

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,69 +1,71 @@
 class StarRating extends HTMLElement {
-    get value () {
-        return this.getAttribute('value') || 0;
+  get value() {
+    return this.getAttribute('value') || 0;
+  }
+
+  set value(val) {
+    this.setAttribute('value', val);
+    this.highlight(this.value - 1);
+  }
+
+  get number() {
+    return this.getAttribute('number') || 5;
+  }
+
+  set number(val) {
+    this.setAttribute('number', val);
+
+    this.stars = [];
+
+    while (this.firstChild) {
+      this.removeChild(this.firstChild);
     }
 
-    set value (val) {
-        this.setAttribute('value', val);
-        this.highlight(this.value - 1);
+    for (let i = 0; i < this.number; i++) {
+      let s = document.createElement('div');
+      s.className = 'star';
+      this.appendChild(s);
+      this.stars.push(s);
     }
 
-    get number () {
-        return this.getAttribute('number') || 5;
-    }
+    this.value = this.value;
+  }
 
-    set number (val) {
-        this.setAttribute('number', val);
+  highlight(index) {
+    this.stars.forEach((star, i) => {
+      star.classList.toggle('full', i <= index);
+    });
+  }
 
-        this.stars = [];
+  constructor() {
+    super();
+    this.number = this.number;
 
-        while (this.firstChild) {
-            this.removeChild(this.firstChild);
-        }
+    this.addEventListener('mousemove', e => {
+      let box = this.getBoundingClientRect(),
+        starIndex = Math.floor(
+          ((e.pageX - box.left) / box.width) * this.stars.length
+        );
 
-        for (let i = 0; i < this.number; i++) {
-            let s = document.createElement('div');
-            s.className = 'star';
-            this.appendChild(s);
-            this.stars.push(s);
-        }
+      this.highlight(starIndex);
+    });
 
-        this.value = this.value;
-    }
+    this.addEventListener('mouseout', () => {
+      this.value = this.value;
+    });
 
-    highlight (index) {
-        this.stars.forEach((star, i) => {
-            star.classList.toggle('full', i <= index);
-        });
-    }
+    this.addEventListener('click', e => {
+      let box = this.getBoundingClientRect(),
+        starIndex = Math.floor(
+          ((e.pageX - box.left) / box.width) * this.stars.length
+        );
 
-    constructor () {
-        super();
+      this.value = starIndex + 1;
 
-        this.number = this.number;
-
-        this.addEventListener('mousemove', e => {
-            let box = this.getBoundingClientRect(),
-                starIndex = Math.floor((e.pageX - box.left) / box.width * this.stars.length);
-
-            this.highlight(starIndex);
-        });
-
-        this.addEventListener('mouseout', () => {
-            this.value = this.value;
-        });
-
-        this.addEventListener('click', e => {
-            let box = this.getBoundingClientRect(),
-                starIndex = Math.floor((e.pageX - box.left) / box.width * this.stars.length);
-
-            this.value = starIndex + 1;
-
-            let rateEvent = new Event('rate');
-            this.dispatchEvent(rateEvent);
-        });
-    }
+      let rateEvent = new Event('rate');
+      this.dispatchEvent(rateEvent);
+    });
+  }
 }
-
 
 customElements.define('x-star-rating', StarRating);
