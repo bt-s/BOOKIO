06b25bcd1c827a5a4644f318e79365788d8f855c
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-03-21 14:03:18 +0100

    separate the page into several components

--- src/components/AddNewBookForm/AddNewBookForm.js
+++ src/components/AddNewBookForm/AddNewBookForm.js
@@ -1,142 +1,49 @@
-import React, {Component} from 'react';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import uuid from 'uuid/v1';
-import {throttle} from '../../helpers/utils';
+import React, { useEffect } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-import {addNewUserBook} from '../../redux/actions/addNewUserBook';
-import {fetchBookTitleSuggestions} from '../../redux/actions/bookTitleSuggestions';
+import { changeNewBook } from '../../redux/actions/addNewUserBook';
 
 import Button from '../Button/Button';
 
-class AddNewBookForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newBook: {
-        title: '',
-        owner: '',
-        rating: '',
-        description: '',
-        author: '',
-        location: {},
-        imageURL: ''
-      },
-      suggestions: []
-    };
-    this.getBookTitle = throttle(
-      query => this.props.fetchBookTitleSuggestions(query),
-      700
-    );
-  }
+const AddNewBookForm = props => {
+  const { newBook, changeNewBook } = props;
 
-  /**
-   * @function handleSubmit
-   * @description handle all to be submitted data and pass it to redux action
-   */
-  handleSubmit = e => {
-    e.preventDefault();
+  const handleChangeForm = e => {
+    changeNewBook(e.target);
   };
 
-  handleTitleSuggestions = e => {
-    this.setState({
-      ...this.state,
-      newBook: {
-        ...this.state.newBook,
-        title: e.target.value
-      }
-    });
-    this.getBookTitle(e.target.value);
-  };
-
-  /**
-   * @function handleChangeForm
-   * @description change state value when there is a change in form
-   */
-  handleChangeForm = e => {
-    this.setState({
-      ...this.state,
-      newBook: {
-        ...this.state.newBook,
-        [e.target.name]: e.target.value
-      }
-    });
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     // TODO: Get location from props and set the location state here
     // TODO: set the owner state with ID from local storage
+  })
 
-    // Below is just dummy to set location and ID from local storage
-    this.setState({
-      ...this.state,
-      newBook: {
-        ...this.state.newBook,
-        owner: uuid(),
-        location: {}
-      }
-    });
-  }
+  return (
+    <React.Fragment>
+      <textarea
+        placeholder="Description"
+        name="description"
+        type="text"
+        value={newBook.description}
+        onChange={handleChangeForm}
+      />
 
-  render() {
-    const {newBook} = this.state;
-
-    return (
-      <React.Fragment>
-        <h1> Add New Book Page</h1>
-
-        <form onSubmit={this.handleSubmit}>
-          {/* TODO: Drop to upload image */}
-          <label>
-            Title:
-            <input
-              placeholder="Book Title"
-              name="title"
-              type="text"
-              value={newBook.title}
-              onChange={this.handleTitleSuggestions}
-            />
-          </label>
-          <label>
-            Description:
-            <textarea
-              placeholder="Description"
-              name="description"
-              type="text"
-              value={newBook.description}
-              onChange={this.handleChangeForm}
-            />
-          </label>
-
-          <Button type="submit" text="Add New Book" />
-          {newBook.title}
-        </form>
-        {this.props.isLoading ? (
-          <h3>Loading...</h3>
-        ) : (
-          <ul>
-            {this.props.bookTitleSuggestions.map((suggestion, index) => {
-              return <li key={index}>{suggestion.bookTitleBare}</li>;
-            })}
-          </ul>
-        )}
-      </React.Fragment>
-    );
-  }
+      <Button type="submit" text="Add New Book" />
+  
+    </React.Fragment>
+  );
 }
 
 const mapStateToProps = state => {
   return {
-    bookTitleSuggestions: state.bookState.bookTitleSuggestions,
-    isLoading: state.bookState.isLoading
+    newBook: state.userBookState.newBook
   };
 };
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      fetchBookTitleSuggestions,
-      addNewUserBook
+      changeNewBook
     },
     dispatch
   );
