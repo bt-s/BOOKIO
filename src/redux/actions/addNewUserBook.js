479cc2ebc6af2e1d5c582907ca93cc08d485af08
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-03-21 14:02:24 +0100

    modify redux to handle add new book

--- src/redux/actions/addNewUserBook.js
+++ src/redux/actions/addNewUserBook.js
@@ -1,27 +1,43 @@
-import axios from 'axios';
+// import axios from 'axios';
 import {
-  ADD_NEW_USER_BOOK_ERROR,
-  ADD_NEW_USER_BOOK_LOADING,
-  ADD_NEW_USER_BOOK_SUCCESS
+  // ADD_NEW_USER_BOOK_ERROR,
+  // ADD_NEW_USER_BOOK_LOADING,
+  // ADD_NEW_USER_BOOK_SUCCESS,
+  CHANGE_NEW_BOOK_TITLE,
+  CHANGE_NEW_BOOK
 } from '../constants/action-types';
 
-const loading = () => {
+// const loading = () => {
+//   return {
+//     type: ADD_NEW_USER_BOOK_LOADING
+//   };
+// };
+
+// const success = () => {
+//   return {
+//     type: ADD_NEW_USER_BOOK_SUCCESS
+//   };
+// };
+
+// const error = () => {
+//   return {
+//     type: ADD_NEW_USER_BOOK_ERROR
+//   };
+// };
+
+export const changeNewBookTitle = payload => {
   return {
-    type: ADD_NEW_USER_BOOK_LOADING
+    type: CHANGE_NEW_BOOK_TITLE,
+    payload
   };
 };
 
-const success = () => {
+export const changeNewBook = payload => {
   return {
-    type: ADD_NEW_USER_BOOK_SUCCESS
-  };
-};
-
-const error = () => {
-  return {
-    type: ADD_NEW_USER_BOOK_ERROR
-  };
-};
+    type: CHANGE_NEW_BOOK,
+    payload
+  }
+}
 
 export const addNewUserBook = payload => {
   return dispatch => {
