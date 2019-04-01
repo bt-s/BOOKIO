7209289550b8d1a3afb16921f3417f5a02104b1f
GnimOay <yyaomingm@gmail.com>
2019-04-01 19:12:21 +0200

    Create and style Message in History Page

--- src/components/History/Components.js
+++ src/components/History/Components.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import reactJPG from '../../images/fullstackreact.jpg';
+
 import {UserLabel} from '../Books/Components';
 
 const ItemInfo = props => {
@@ -10,28 +11,36 @@ const ItemInfo = props => {
       <img src={reactJPG} alt="item img" />
       <div className="part-b">
         <div className="title-and-status">
-          <div className="title" />
-          <div className="status" />
+          <div className="title">{props.title}</div>
+          <div className="status">Requesting</div>
         </div>
-        <div className="supplement" />
+        <div className="supplement">1-23 Mar. 2019</div>
       </div>
     </div>
   );
+};
+ItemInfo.defaultProps = {
+  title: 'The Great Gatsby'
 };
 
 const RequestMessage = props => {
   return (
     <div className="request-msg">
       <div className="msg-time">11 Mar. 2019, 14:22</div>
-      <ItemInfo />
-      <div className="people-and-peration">
-        <div className="type-and-people">
-          <div className="type" />
-          <Link to="/account" className="user-container">
-            <UserLabel />
-          </Link>
+      <div className="msg-body">
+        <ItemInfo />
+        <div className="people-and-operation">
+          <div className="type-and-people">
+            <div className="type">Request to borrow</div>
+            <Link to="/account" className="user-container">
+              <UserLabel />
+            </Link>
+          </div>
+          <div className="operation">
+            <button className="btn decline">Decline</button>
+            <button className="btn accept">Accept</button>
+          </div>
         </div>
-        <div className="operation" />
       </div>
       <div className="msg-cut-off-line">---------------</div>
     </div>
