dbe25e09171df777f1aba37ff8bcf5b590389ad7
Ria Ratna Sari <rrsari@Rias-MacBook-Pro.local>
2019-04-16 00:12:42 +0200

    change homepage style

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,15 @@ import {Link} from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 
 import {withFirebase} from '../components/Firebase';
+import {
+  faPhone,
+  faEnvelope,
+  faHome,
+  faBirthdayCake,
+  faUserShield,
+} from '@fortawesome/free-solid-svg-icons';
 
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 const HomePage = props => {
   const bannerSrcSet = [
     ''.concat(
@@ -20,35 +28,72 @@ const HomePage = props => {
     ''.concat(
       require('../images/homepage/book-stack-min-2400x600.jpg'),
       ' 1400w'
-    )
+    ),
   ].join(',');
 
   return (
     <React.Fragment>
       <img
         className="home-img-banner"
-        srcSet={bannerSrcSet}
-        src={require('../images/homepage/book-stack-min-1400x467.jpg')}
+        src={require('../images/bookstack.jpg')}
         alt="Book stack homepage banner"
       />
+
       <div className="home-banner-txt-container">
         <div className="inner">
-          <h1>
-            <span className="left">BOOK</span>
-            <span className="right">IO</span>
-          </h1>
-          <h2>Free book-sharing made easy</h2>
+          <h1>Join the largest book sharing platform in the world</h1>
+          <div>
+            BOOKIO connects the people who want to give their books away or lend
+            their books.
+          </div>
+          <br />
+          If you want to read a book without paying it or want to give or lend
+          the book, BOOKIO is for you
+          <br />
+          <br />
+          <br />
           <Link to={ROUTES.SIGN_UP} className="btn btn-orange btn-signup">
-            Register now
+            Join Now
           </Link>
         </div>
       </div>
+
+      <div className="field-item center">
+        <div>
+          <div className="inner-card">
+            <h2>Secure</h2>
+            All your transacton is protected with compliance procedure
+          </div>
+        </div>
+
+        <div>
+          <div className="inner-card">
+            <h2>Easy</h2>
+            Find your favorite book, request and get the book within 5 minutes
+          </div>
+        </div>
+
+        <div>
+          <div className="inner-card">
+            <h2>Efficient</h2>
+            Easy to track your booking history and get reminder for every action
+          </div>
+        </div>
+        <div>
+          <div className="inner-card">
+            <h2>Search</h2>
+            Lot of books from different authors with different languages
+          </div>
+        </div>
+      </div>
+
+      <h1>How BOOKIO works?</h1>
     </React.Fragment>
   );
 };
 
 HomePage.propTypes = {
-  firebase: PropTypes.object
+  firebase: PropTypes.object,
 };
 
 export default compose(withFirebase)(HomePage);
