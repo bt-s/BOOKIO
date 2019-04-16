cfb40836db9f59c8852f5851b0379ec698f66e84
Ria Ratna Sari <rrsari@n161-p49.eduroam.kth.se>
2019-04-16 16:53:25 +0200

    add how it works

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -35,59 +35,105 @@ const HomePage = props => {
     <React.Fragment>
       <img
         className="home-img-banner"
-        src={require('../images/bookstack.jpg')}
+        src={require('../images/reading.png')}
         alt="Book stack homepage banner"
       />
 
       <div className="home-banner-txt-container">
         <div className="inner">
           <h1>Join the largest book sharing platform in the world</h1>
-          <div>
-            BOOKIO connects the people who want to give their books away or lend
-            their books.
+          <div className="inner-content">
+            BOOKIO is a leading book sharing platform in the world. It connects
+            the people who want to give or lend their books away and the people
+            who want to read the book without paying it. If you want to
+            contribute for a sustainable environment and improve the world
+            literacy, BOOKIO is for you.
           </div>
           <br />
-          If you want to read a book without paying it or want to give or lend
-          the book, BOOKIO is for you
+
           <br />
-          <br />
-          <br />
-          <Link to={ROUTES.SIGN_UP} className="btn btn-orange btn-signup">
+          <Link to={ROUTES.SIGN_UP} className="btn btn-dark-orange btn-signup">
             Join Now
           </Link>
         </div>
       </div>
 
-      <div className="field-item center">
+      <div className="field-item center search-result">
         <div>
           <div className="inner-card">
             <h2>Secure</h2>
-            All your transacton is protected with compliance procedure
+            <div className="line-break" />
+            <br />
+            We ensure that every transaction is secure with compliance
+            procedure.
           </div>
         </div>
 
         <div>
           <div className="inner-card">
             <h2>Easy</h2>
-            Find your favorite book, request and get the book within 5 minutes
+            <div className="line-break" />
+            <br />
+            <div>
+              Find your favorite book, request to owner, and let you decide when
+              you want to pick it up.
+            </div>
           </div>
         </div>
 
         <div>
           <div className="inner-card">
             <h2>Efficient</h2>
-            Easy to track your booking history and get reminder for every action
+            <div className="line-break" />
+            <br />
+            Track your booking without hassle. You will be notified for every
+            process.
           </div>
         </div>
         <div>
           <div className="inner-card">
             <h2>Search</h2>
-            Lot of books from different authors with different languages
+            <div className="line-break" />
+            <br />
+            We provide lot of books from different authors with different
+            languages
           </div>
         </div>
       </div>
 
-      <h1>How BOOKIO works?</h1>
+      <div className="user-information">
+        <img
+          className="mockup-platform"
+          src={require('../images/stevejobs.jpg')}
+          alt="Book stack homepage banner"
+        />
+
+        <div className="how-it-works">
+          <h1>How BOOKIO works?</h1>
+          <div>
+            <div className="header-title">Find your book.</div>
+            Search the book that you want to read. BOOKIO provides a fully
+            comprehensive books from various authors provided in different
+            languages.
+          </div>
+          <div>
+            <div className="header-title">Request the book</div>
+            Request to lend or get the book from the book's owner. We let the
+            owner decide to lend or give their books away.
+          </div>
+          <div>
+            <div className="header-title">Meet the owner</div>
+            Set the appointment to meet the owner to pickup the book. Our
+            platform will only give the owner detail location once the request
+            is approved.
+          </div>
+          <div>
+            <div className="header-title">Return the book</div>
+            Return the book on time to let other customer read the book you.
+            like. We will remind you when you need to return the book.
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
