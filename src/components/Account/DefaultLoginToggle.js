b7954540d75bf7d0b3f62b68692887b99d06abca
Bas Straathof <bas_straathof@msn.com>
2019-04-20 09:31:43 +0200

    Fix console warnings

--- src/components/Account/DefaultLoginToggle.js
+++ src/components/Account/DefaultLoginToggle.js
@@ -1,40 +1,10 @@
-import React, {useRef, useReducer} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
-import {Validation, Validator, ValidationHelper} from '../Forms/Validation';
-import {formReducer, errorReducer} from '../../helpers/validationHelper';
 
 import Button from '../Button/Button';
 
 const DefaultLoginToggle = props => {
   const {onlyOneLeft, isEnabled, signInMethod, onUnlink} = props;
-
-  const initialFormValues = () => ({
-    passwordOne: '',
-    passwordTwo: ''
-  });
-
-  const validationRef = useRef(null);
-
-  const [form, dispatchForm] = useReducer(formReducer, {}, initialFormValues);
-  const [error, dispatchError] = useReducer(errorReducer, {});
-
-  const handleChange = e => {
-    dispatchForm({
-      name: e.target.name,
-      value: e.target.value
-    });
-  };
-
-  const onValidate = error => {
-    dispatchError(error);
-  };
-
-  const onSubmit = e => {
-    e.preventDefault();
-    validationRef.current.validate();
-    props.onLink(form.passwordOne);
-  };
 
   return (
     isEnabled && (
